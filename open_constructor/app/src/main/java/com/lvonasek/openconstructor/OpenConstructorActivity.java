8c5fb9b2bdc425422b611bfacbbe6ba9979a5a1f
lubos <lubos@mapfactor.com>
2017-01-04 10:06:29 +0100

    Viewer mode implemented

--- open_constructor/app/src/main/java/com/lvonasek/openconstructor/OpenConstructorActivity.java
+++ open_constructor/app/src/main/java/com/lvonasek/openconstructor/OpenConstructorActivity.java
@@ -29,6 +29,7 @@ import android.graphics.Point;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.view.Display;
 import android.view.MotionEvent;
@@ -36,7 +37,9 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,17 +47,21 @@ import java.io.File;
 
 public class OpenConstructorActivity extends Activity implements View.OnClickListener {
 
+  private static final String MODEL_DIRECTORY = "/Models/";
   private static final int REQUEST_CODE_PERMISSION_CAMERA = 1987;
   private static final int REQUEST_CODE_PERMISSION_STORAGE = 1988;
 
   private ProgressBar mProgress;
   private OpenConstructorRenderer mRenderer;
   private GLSurfaceView mGLView;
+  private SeekBar mSeekbar;
 
+  private LinearLayout mLayoutRecBottom;
   private Button mClearButton;
   private Button mSaveButton;
   private Button mToggleButton;
 
+  private LinearLayout mLayoutRecTop;
   private TextView mResText;
   private Button mResPlus;
   private Button mResMinus;
@@ -64,6 +71,7 @@ public class OpenConstructorActivity extends Activity implements View.OnClickLis
   private GestureDetector mGestureDetector;
   private float mMoveX = 0;
   private float mMoveY = 0;
+  private float mPitch = 0;
   private float mYaw = 0;
   private float mZoom = 0;
 
@@ -94,61 +102,46 @@ public class OpenConstructorActivity extends Activity implements View.OnClickLis
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     // Setup UI elements and listeners.
+    mLayoutRecBottom = (LinearLayout) findViewById(R.id.layout_rec_bottom);
     mClearButton = (Button) findViewById(R.id.clear_button);
     mClearButton.setOnClickListener(this);
-
     mToggleButton = (Button) findViewById(R.id.toggle_button);
     mToggleButton.setOnClickListener(this);
-
     mSaveButton = (Button) findViewById(R.id.save_button);
-    mSaveButton.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        //pause
-        m3drRunning = false;
-        TangoJNINative.onToggleButtonClicked(m3drRunning);
-        refreshUi();
-        //save
-        setupPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_CODE_PERMISSION_STORAGE);
-      }
-    });
+    mSaveButton.setOnClickListener(this);
 
+    mLayoutRecTop = (LinearLayout) findViewById(R.id.layout_rec_top);
     mResText = (TextView) findViewById(R.id.res_text);
     mResPlus = (Button) findViewById(R.id.res_plus);
+    mResPlus.setOnClickListener(this);
     mResMinus = (Button) findViewById(R.id.res_minus);
-
-    mResPlus.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        mRes++;
-        if(mRes > 10)
-          mRes = 10;
-        refresh3dr();
-        refreshUi();
-      }
-    });
-    mResMinus.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        mRes--;
-        if(mRes <= -1)
-          mRes = -1;
-        refresh3dr();
-        refreshUi();
-      }
-    });
+    mResMinus.setOnClickListener(this);
 
     // OpenGL view where all of the graphics are drawn
     mGLView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
     mGLView.setEGLContextClientVersion(2);
     mGLView.setRenderer(mRenderer);
     mProgress = (ProgressBar) findViewById(R.id.progressBar);
+    mSeekbar = (SeekBar) findViewById(R.id.seekBar);
+    mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+    {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int value, boolean byUser)
+      {
+        mPitch = (float) Math.toRadians(-value);
+        TangoJNINative.setView(mYaw, mPitch, mMoveX, mMoveY, !mViewMode);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar)
+      {
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar)
+      {
+      }
+    });
     refreshUi();
 
     mGestureDetector = new GestureDetector(new GestureDetector.GestureListener()
@@ -160,14 +153,14 @@ public class OpenConstructorActivity extends Activity implements View.OnClickLis
         float f = getMoveFactor();
         mMoveX += dx * f * Math.cos( angle ) + dy * f * Math.sin( angle );
         mMoveY += dx * f * Math.sin( angle ) - dy * f * Math.cos( angle );
-        TangoJNINative.setView(mYaw, -90, mMoveX, mMoveY, !mViewMode);
+        TangoJNINative.setView(mYaw, mPitch, mMoveX, mMoveY, !mViewMode);
       }
 
       @Override
       public void OnRotation(float angle)
       {
-        mYaw = -angle * (float)Math.PI / 180.0f;
-        TangoJNINative.setView(mYaw, -90, mMoveX, mMoveY, !mViewMode);
+        mYaw = (float) Math.toRadians(-angle);
+        TangoJNINative.setView(mYaw, mPitch, mMoveX, mMoveY, !mViewMode);
       }
 
       @Override
@@ -203,6 +196,28 @@ public class OpenConstructorActivity extends Activity implements View.OnClickLis
             TangoJNINative.onClearButtonClicked();
           }
         });
+      break;
+    case R.id.save_button:
+      //pause
+      m3drRunning = false;
+      TangoJNINative.onToggleButtonClicked(false);
+      refreshUi();
+      //save
+      setupPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_CODE_PERMISSION_STORAGE);
+      break;
+    case R.id.res_minus:
+      mRes--;
+      if(mRes <= -1)
+        mRes = -1;
+      refresh3dr();
+      refreshUi();
+      break;
+    case R.id.res_plus:
+      mRes++;
+      if(mRes > 10)
+        mRes = 10;
+      refresh3dr();
+      refreshUi();
       break;
     }
 
@@ -322,13 +337,7 @@ public class OpenConstructorActivity extends Activity implements View.OnClickLis
                       builder.setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                          //TODO:Viewer mode
-                          mMoveX = 0;
-                          mMoveY = 0;
-                          mYaw = 0;
-                          mZoom = 10;
-                          mViewMode = true;
-                          TangoJNINative.setView(mYaw, -90, mMoveX, mMoveY, !mViewMode);
+                          setViewerMode();
                           dialog.cancel();
                         }
                       });
@@ -379,8 +388,22 @@ public class OpenConstructorActivity extends Activity implements View.OnClickLis
   }
 
   private String getPath() {
-    String dir = "/mnt/sdcard/Models/";
+    String dir = Environment.getExternalStorageDirectory().getPath() + MODEL_DIRECTORY;
     new File(dir).mkdir();
     return dir;
+  }
+
+  private void setViewerMode()
+  {
+    mLayoutRecBottom.setVisibility(View.GONE);
+    mLayoutRecTop.setVisibility(View.GONE);
+    mSeekbar.setVisibility(View.VISIBLE);
+    mMoveX = 0;
+    mMoveY = 0;
+    mYaw = 0;
+    mZoom = 10;
+    mViewMode = true;
+    mSeekbar.setProgress(90);
+    TangoJNINative.setZoom(mZoom);
   }
 }
